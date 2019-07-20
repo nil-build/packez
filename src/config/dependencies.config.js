7478c2a9486d1734b76626dd46b064dff374d718
bplok20010 <nobo.zhou@foxmail.com>
2019-07-20 23:55:39 +0800

    1. mode=production下支持css压缩

--- src/config/dependencies.config.js
+++ src/config/dependencies.config.js
@@ -1,33 +1,23 @@
 module.exports = {
-    "babel": [
-        "babel-loader",
-        "babel-preset-packez",
-    ],
-    "css": [
+    babel: ["babel-loader", "babel-preset-packez"],
+    css: [
         "style-loader",
         "css-loader",
         "postcss-loader",
         "mini-css-extract-plugin",
+        "optimize-css-assets-webpack-plugin",
+        "postcss-safe-parser",
         "postcss-flexbugs-fixes",
         "postcss-preset-env"
     ],
-    "less": [
-        "less",
-        "less-loader",
-    ],
-    "sass": [
-        "node-sass",
-        "sass-loader"
-    ],
-    "scss": [
-        "node-sass",
-        "sass-loader"
-    ],
-    "eslint": [
+    less: ["less", "less-loader"],
+    sass: ["node-sass", "sass-loader"],
+    scss: ["node-sass", "sass-loader"],
+    eslint: [
         "eslint",
         "babel-eslint",
         "eslint-loader",
-        "eslint-config-packez",
+        "eslint-config-packez"
         // "eslint-plugin-flowtype",
         // "eslint-plugin-import",
         // "eslint-plugin-import",
@@ -37,11 +27,6 @@ module.exports = {
         // "eslint-config-alloy",
         // "eslint-config-airbnb",
     ],
-    "json5": [
-        "json5-loader"
-    ],
-    "vue": [
-        "vue-loader",
-        "vue-template-compiler"
-    ]
-}
+    json5: ["json5-loader"],
+    vue: ["vue-loader", "vue-template-compiler"]
+};
--- src/webpack/webpack.config.optimization.js
+++ src/webpack/webpack.config.optimization.js
@@ -1,18 +1,61 @@
-import { defaultsDeep } from 'lodash';
+import { defaultsDeep } from "lodash";
+import TerserPlugin from "terser-webpack-plugin";
+import OptimizeCSSAssetsPlugin from "optimize-css-assets-webpack-plugin";
+import safePostCssParser from "postcss-safe-parser";
 
-module.exports = function (opts) {
+module.exports = function(opts) {
+    const shouldUseSourceMap = opts.shouldUseSourceMap;
+    const isEnvProduction = opts.mode === "production";
 
     return defaultsDeep(
         {
-            nodeEnv: opts.mode,
+            nodeEnv: opts.mode
         },
         opts.optimization || {},
         {
+            minimize: isEnvProduction,
+            minimizer: [
+                new TerserPlugin({
+                    terserOptions: {
+                        parse: {
+                            ecma: 8
+                        },
+                        compress: {
+                            ecma: 5,
+                            warnings: false,
+                            comparisons: false,
+                            inline: 2
+                        },
+                        mangle: {
+                            safari10: true
+                        },
+                        output: {
+                            ecma: 5,
+                            comments: false,
+                            ascii_only: true
+                        }
+                    },
+                    parallel: true,
+                    cache: true,
+                    sourceMap: shouldUseSourceMap
+                }),
+                new OptimizeCSSAssetsPlugin({
+                    cssProcessorOptions: {
+                        parser: safePostCssParser,
+                        map: shouldUseSourceMap
+                            ? {
+                                  inline: false,
+                                  annotation: true
+                              }
+                            : false
+                    }
+                })
+            ],
             runtimeChunk: true,
             splitChunks: {
-                chunks: 'all',
-                name: false,
+                chunks: "all",
+                name: false
             }
         }
     );
-}
+};
